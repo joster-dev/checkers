e1922fa54ab1f4b14b40af932a9a1ea15f21be4d
Joster422 <Joster422@gmail.com>
2020-05-23 22:05:03 -0400

    better names

--- src/app/game/form/form.component.ts
+++ src/app/game/form/form.component.ts
@@ -2,12 +2,12 @@ import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { Form } from 'src/app/models';
 
 @Component({
-  selector: 'checkers-form[form]',
+  selector: 'checkers-form[model]',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
-  @Input() form!: Form;
+  @Input() model!: Form;
 
   @Output() restart = new EventEmitter();
 
